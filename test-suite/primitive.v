dbe9c5c98884d22377d214fd97a6301e47dea499
Jan-Oliver Kaiser <janno@mpi-sws.org>
2020-04-29 15:55:52 +0200

    Add prim. proj. test case with parameters.
    Also restructure test-suite/primitive.v a bit.

--- test-suite/primitive.v
+++ test-suite/primitive.v
@@ -2,17 +2,34 @@ From Unicoq Require Import Unicoq.
 
 Set Primitive Projections.
 
-Record cs := { ty : Type; data : nat }.
-Canonical Structure cs_unit := {| ty := unit; data:=0 |}.
+Module Test1.
 
-(* The definition below used to fail with:
+  Record cs := { ty : Type }.
+  Canonical Structure cs_unit := {| ty := unit |}.
+
+  (* The definition below used to fail with:
 
 Error: In environment
 c := ?c : cs
 The term "tt" has type "unit" while it is expected to have type "ty c".
 
- *)
-Definition x :=
-  let c : cs := _ in
-  let x := (fun (u : ty c) => u) (tt) in
-  data c.
+   *)
+  Definition x :=
+    let c : cs := _ in
+    let x := (fun (u : ty c) => u) (tt) in
+    c.
+
+End Test1.
+
+(* Similar to Test1 but with parameters *)
+Module Test2.
+
+  Record cs (A : Type) := { ty : Type }.
+  Canonical Structure cs_unit {A} : cs A := {| ty := unit |}.
+
+  Definition x :=
+    let c : cs nat := _ in
+    let x := (fun (u : ty _ c) => u) (tt) in
+    c.
+
+End Test2.
