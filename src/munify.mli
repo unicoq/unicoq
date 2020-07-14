777c6d6422d151c61ab7d073c520de89d59d25e3
beta <beta@mpi-sws.org>
2020-07-13 22:56:07 -0300

    adding options

--- src/munify.mli
+++ src/munify.mli
@@ -11,6 +11,17 @@ type unify_fun =
   Environ.env -> Evd.evar_map ->
   Reduction.conv_pb -> EConstr.t -> EConstr.t -> Evarsolve.unification_result
 
+type options = {
+    inst_beta_reduce_type : bool;
+    inst_unify_types : bool;
+    inst_aggressive : bool;
+    inst_super_aggressive : bool;
+    inst_try_solving_eqn : bool;
+    use_hash : bool
+}
+
+val current_options : unit -> options
+
 val unify_evar_conv : TransparentState.t -> unify_fun
 
 (** Given a set of evars s and terms t1 t2, it unifies the terms only
@@ -27,7 +38,7 @@ val unify_match_nored : Evar.Set.t -> TransparentState.t -> unify_fun
 
 (** Instantiates an evar `?x[subst] args` with a term `t` *)
 val instantiate : ?conv_t:Reduction.conv_pb ->
-                  ?unify_types:bool ->
+                  ?options: options ref ->
                   Environ.env ->
                   EConstr.t Constr.pexistential * EConstr.t list ->
                   EConstr.t ->
